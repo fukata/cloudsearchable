4ec83652d894cf3a901e329d853a1b3563cac4b5
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2015-09-15 17:02:28 +0700

    add literal_array option

--- lib/cloudsearchable/field.rb
+++ lib/cloudsearchable/field.rb
@@ -11,20 +11,21 @@ module Cloudsearchable
   class Field
     FieldTypes = [:literal, :int, :int_array, :text, :text_array, :date, :double, :double_array, :latlon].freeze
     # Maps the type of field to the name of the options hash when defining the field
-    FieldTypeOptionsNames = {:literal => :literal_options, :int => :int_options, :int_array => :int_array_options, :text => :text_options, :text_array => :text_array_options, 
+    FieldTypeOptionsNames = {:literal => :literal_options, :literal_array => :literal_array_options, :int => :int_options, :int_array => :int_array_options, :text => :text_options, :text_array => :text_array_options, 
       :date => :date_options, :date_array => :date_array_options, :double => :double_options, :double_array => :double_array_options, :latlon => :lat_lon_options}.freeze
     # Maps from field type to the allowed set of options for the field
     FieldTypeOptionsKeys = {
-      literal:      [:default_value, :facet_enabled, :search_enabled, :result_enabled, :sort_enabled].freeze,
-      int:          [:default_value, :facet_enabled, :search_enabled, :result_enabled, :sort_enabled].freeze,
-      int_array:    [:default_value, :facet_enabled, :search_enabled, :result_enabled].freeze,
-      text:         [:default_value, :facet_enabled, :result_enabled, :sort_enabled, :highlight_enabled].freeze,
-      text_array:   [:default_value, :facet_enabled, :result_enabled, :highlight_enabled].freeze,
-      date:         [:default_value, :facet_enabled, :result_enabled, :sort_enabled].freeze,
-      date_array:   [:default_value, :facet_enabled, :result_enabled].freeze,
-      double:       [:default_value, :facet_enabled, :result_enabled, :sort_enabled].freeze,
-      double_array: [:default_value, :facet_enabled, :result_enabled].freeze,
-      latlon:       [:default_value, :facet_enabled, :result_enabled, :sort_enabled].freeze,
+      literal:       [:default_value, :facet_enabled, :search_enabled, :result_enabled, :sort_enabled].freeze,
+      literal_array: [:default_value, :facet_enabled, :search_enabled, :result_enabled].freeze,
+      int:           [:default_value, :facet_enabled, :search_enabled, :result_enabled, :sort_enabled].freeze,
+      int_array:     [:default_value, :facet_enabled, :search_enabled, :result_enabled].freeze,
+      text:          [:default_value, :facet_enabled, :result_enabled, :sort_enabled, :highlight_enabled].freeze,
+      text_array:    [:default_value, :facet_enabled, :result_enabled, :highlight_enabled].freeze,
+      date:          [:default_value, :facet_enabled, :result_enabled, :sort_enabled].freeze,
+      date_array:    [:default_value, :facet_enabled, :result_enabled].freeze,
+      double:        [:default_value, :facet_enabled, :result_enabled, :sort_enabled].freeze,
+      double_array:  [:default_value, :facet_enabled, :result_enabled].freeze,
+      latlon:        [:default_value, :facet_enabled, :result_enabled, :sort_enabled].freeze,
     }.freeze
     attr_reader :name, :type, :source, :options
 
